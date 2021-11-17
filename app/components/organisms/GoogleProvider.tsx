f60f52d0a6e2f664979c7b7261270cc4510d8462
DerekZohar <62027015+DerekZohar@users.noreply.github.com>
2021-11-17 15:06:13 +0700

    Update GoogleProvider.tsx

--- app/components/organisms/GoogleProvider.tsx
+++ app/components/organisms/GoogleProvider.tsx
@@ -5,8 +5,7 @@ const GoogleAuthContext = React.createContext(null);
 
 export const GoogleAuthProvider = ({ children }) => {
   const googleAuth = useGoogleLogin({
-    clientId:
-      "268679938372-3n67ksgd7atdhus9d5kaheritku4c7in.apps.googleusercontent.com", // Your clientID from Google.
+    clientId: process.env.NEXT_PUBLIC_CLIENT_ID, // Your clientID from Google.
   });
 
   return (
